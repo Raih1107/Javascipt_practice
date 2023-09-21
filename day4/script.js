9b5757b6871684a13fe870739da2ab3dd9bd9392
Raih1107 <raih68737@gmail.com>
2023-09-21 23:58:48 +0530

    Added day4

---
+++ day4/script.js
@@ -0,0 +1,45 @@
+// Function statement or declaration
+
+// a();
+// b();   // Throws an error
+// function a(){
+//     console.log("a called");
+// }
+
+// //Function expression
+// var b = function() {
+//     console.log("b is called");
+// }
+
+// // Named Function
+// var add = function sum(x, y) {
+//     return x + y;
+// };
+
+// console.log(add(2, 3)); // Outputs: 5
+// console.log(sum(2, 3)); // ReferenceError: sum is not defined
+
+
+
+
+//Callback functions
+
+// setTimeout(function () {
+//     console.log("timer");
+// }, 5000);
+
+// function x(y) {
+//     console.log("x");
+//     y();
+// }
+
+// x(function y() {
+//     console.log("y");
+// });
+
+
+
+
+document.getElementById("ClickMe").addEventListener("click", function xyz() {
+console.log("Button Clicked");
+});
